0a8bc24da31e37850987291b541ca9549ac6af8e
james Starks <jamesestarks87@gmail.com>
2022-04-11 20:29:52 -0500

    Added time scale and base band stretch functions

--- digital_modulation/digital_modulation.py
+++ digital_modulation/digital_modulation.py
@@ -1,22 +1,20 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class Modulation():
-  modulation_technique = ""
-  data = ""
-  data_len = 0
-  bb_signal = []
-  fc = []
-  rb = 0
+  RESOLUTION=100
 
   # Constructor
   # Input data string is expected in binary
-  def __init__(self, modulation_technique, data, fc, rb):
+  def __init__(self, modulation_technique, data, fc, rs):
     self.modulation_technique = modulation_technique
     self.data = np.asanyarray(list(data))
     self.data_len = len(self.data)
+
     self.fc = fc
-    self.rb = rb
+    self.tc = 1/fc
+    self.rs = rs
+    self.ts = 1/rs
 
   # Differentiate for binary or bit pairs
   def differential(self, quartenary=False):
@@ -26,7 +24,7 @@ class Modulation():
       pass
   
   # Repackage data into bit pairs dtype string
-  def bit_pair(self):
+  def pair_bits(self):
     # Make data stream even if odd number of bits
     if (self.data_len % 2 is 1):
       self.data = np.append(self.data, self.data[-1])
@@ -39,8 +37,25 @@ class Modulation():
     self.data_len = len(self.data)
  
   # Time stretch base band signal
-  def stretch_bb(self):
-    pass
+  def timescale_base_band_signal(self):
+    base_band_signal = []
+    for data in self.data:
+      if data == "0" or data == "00": amp = 0
+      elif data == "1" or data == "11": amp = 1
+      elif data == "01": amp = 2
+      elif data == "10": amp = 3
+
+      base_band_signal = np.append(base_band_signal,
+                                   np.full(int(self.cycles_per_symbol * self.RESOLUTION), fill_value=amp))
+
+    self.base_band_signal = base_band_signal
+      
+  # Initialize time axis based on resolution, carrier frequency, and data rate
+  def setup_timeaxis(self):
+    self.cycles_per_symbol = self.fc/self.rs
+    stop = self.cycles_per_symbol*self.tc*self.data_len
+    step = self.tc / self.RESOLUTION
+    self.x = np.arange(0, stop, step)
 
   # Amplitude shift keying
   def ask(self):
@@ -69,9 +84,12 @@ class Modulation():
 
 #------------------------Debug------------------------
 def main():
-  mod = Modulation("psk", "11010", 23, 23)
-  print(f"{mod.data_len} {mod.data}")
-  mod.bit_pair()
-  print(f"{mod.data_len} {mod.data}")
+  mod = Modulation("qpsk", "11010", 150E3, 25E3)
+  mod.pair_bits()  
+  mod.setup_timeaxis()
+  mod.timescale_base_band_signal()
+  print(len(mod.x))
+  print(len(mod.base_band_signal))
+
 
 if __name__ == "__main__": main()
