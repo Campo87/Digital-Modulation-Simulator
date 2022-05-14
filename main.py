0b80a691f943df73048bca9c4c290d7b7f6322d9
James Starks <jamesestarks87@gmail.com>
2022-05-14 12:31:12 -0500

    Refactored GUI

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#----------------------------------------------------------------------------
+# Created By  : James Starks & Inioluwa Obisakin
+# Created Date: 5/14/22
+import digital_modulation.digital_modulation_gui as gui
+# ---------------------------------------------------------------------------
+def main():
+  _gui = gui.Digital_Modulation_GUI() # Start GUI
+
+if __name__ == "__main__": main()
